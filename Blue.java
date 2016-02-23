8753fed1456e5730cd7caab17a17c427e8eadd85
Josh Graves <grave121@mail.chapman.edu>
2016-02-23 11:37:24 -0800

    Added files via upload

--- Blue.java
+++ Blue.java
@@ -15,60 +15,90 @@ class UDPClient {
 
  		DatagramPacket initiatePacket = new DatagramPacket(initiateData, initiateData.length, IPAddress, /*some socket address*/);
  		clientSocket.sendPacket(initiatePacket);
+
+ 		sendMessage("HELLO Blue", );
  		
  		//receive a message and put it into a string
  		String firstMessage;
  		boolean firstMessageBool = false;
 
+ 		DatagramPacket receivePacket;
+ 		String receivedMessge;
+ 		String sentence;
+ 		BufferedReader inFromUser;
+
  		while (true) {
 			
 			//receive a message and put it into a string
-			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+			receivePacket = new DatagramPacket(receiveData, receiveData.length);
 			clientSocket.receive(receivePacket);
-			String receivedMessge = new String(receivePacket.getData());
- 		
+			receivedMessge = new String(receivePacket.getData());
+ 			
+ 			//covers if the first message is 100
  			if (receivedMessge.equals("100")) {
  				firstMessage = "100";
  				firstMessageBool = true;
- 				System.out.println("100 Received.");
+ 				System.out.println(firstMessage);
  				continue;
  			}
 
+ 			//covers if received message is goodbye
  			else if (receivedMessge.equals("Goodbye")) {
  				System.out.println("Goodbye");
+ 				clientSocket.close();
  				break;
  			}
 
  			else if (receivedMessge.equals("200") && firstMessageBool) {
+ 				System.out.println("200");
  				/*send first message*/
  			}
 
  			else {
-
+ 				System.out.println(receivedMessge);
  			}
 
 
-			BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
- 			
-			byte[] sendData = new byte[1024];
-			byte[] receiveData = new byte[1024];
+			inFromUser = new BufferedReader(new InputStreamReader(System.in));
+ 			sentence = inFromUser.readLine();
+			sendMessage(sentence, )
 
  		}
-		
-		String sentence = inFromUser.readLine();
-		sendData = sentence.getBytes();
-		
-		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, /*some socket address*/);
 
-		clientSocket.send(sendPacket);
-		
-		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-		
-		clientSocket.receive(receivePacket);
-		
-		String modifiedSentence = new String(receivePacket.getData());
-		
-		System.out.println("FROM SERVER:" + modifiedSentence);
 		clientSocket.close();
 	}	
 }
+
+public static void sendMessage(String message) {
+	
+	//put message to be send in a packet to be sent by the socket
+	byte sendData = new byte[1024];
+	sendData = message.getBytes();
+	DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress /*pass in?*/, /*some socket address, pass in?*/);
+
+	/*pass clientSocket in somehow?*/
+	clientSocket.send(sendPacket);
+}
+
+public static String receiveMessage() {
+
+	//set up the socket to receive the new message
+	byte[] receiveData = new byte[1024];
+	DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+	
+	//have the socket receive the message
+	clientSocket.receive(receivePacket);
+	
+	//make a new string that contains the new message
+	String newMessage = new String(receivePacket.getData());
+	return newMessage;
+
+}
+
+
+
+
+
+
+
+
